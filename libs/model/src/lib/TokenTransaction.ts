d2b2806f5643ba8fc0621814771e5d981a517959
Philip Mutua <pmutua@live.com>
2023-02-10 18:15:04 +0300

    :construction:

---
+++ libs/model/src/lib/TokenTransaction.ts
@@ -0,0 +1,9 @@
+export interface TokenTransaction{
+  id: number; // A unique identifier for each transaction. This could be an automatically generated ID or a combination of the user ID and timestamp.
+  userId: string; // A foreign key referencing the user_id field in the Users collection. This field would indicate which user the transaction belongs to.
+  date: Date; // The date and time of the transaction.
+  tokensDeducted: number; // The number of tokens deducted from the user's balance for the transaction.
+  
+  reason: string; //  A brief description of why the tokens were deducted. This could be something like "purchased item", "used service", etc.
+
+}

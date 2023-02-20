a87775257475c017507af2e330b6af2bbfd4a23a
Philip Mutua <pmutua@live.com>
2023-02-20 21:38:57 +0300

    fix: circular dependency

--- libs/utilities/src/lib/utilities.module.ts
+++ libs/utilities/src/lib/utilities.module.ts
@@ -1,11 +1,6 @@
 import { CommonModule } from '@angular/common';
 // eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
 import { NgModule } from '@angular/core';
-import { SharedModule } from '@andika/libs/shared';
-
-import { AuthService } from './firebase/services/auth.service';
-import { FormsModule } from '@angular/forms';
-
 @NgModule({
   declarations: [
   ],

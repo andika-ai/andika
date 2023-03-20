787f15638c8cac6c03d7861929e06388f8faa014
Philip Mutua <pmutua@live.com>
2023-03-20 08:47:56 +0300

    fix: only the pages that are relevant to my account.closes #106

--- libs/elements/src/lib/components/main-navbar/main-navbar.component.ts
+++ libs/elements/src/lib/components/main-navbar/main-navbar.component.ts
@@ -1,16 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@andika/libs/utilities';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import {Router} from "@angular/router";
+
+import {
+  faTeletype,
+  faTimes,
+  faExpand,
+  faQuestionCircle,
+  faLightbulb,
+  faPen,
+  faPenNib,
+  faClockRotateLeft,
+  faUser,
+  faBackwardStep,
+  faArrowLeft
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-main-navbar',
   templateUrl: './main-navbar.component.html',
   styleUrls: ['./main-navbar.component.scss']
 })
-export class MainNavbarComponent implements OnInit {
-
-  constructor(private _router: Router) { }
+export class MainNavbarComponent implements OnInit, AfterViewInit {
+  showOptions = false;
+  faArrowLeft = faArrowLeft;
+  constructor(private _router: Router, private _authService: AuthService) {}
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit(): void {
+  }
+
+  loggedIn(){
+    return this._authService.isLoggedIn;
   }
 
   navigateToLogin() {
@@ -19,6 +42,20 @@ export class MainNavbarComponent implements OnInit {
 
   navigateToRegister() {
     this._router.navigate(['/register']);
+  }
+
+  /**
+   * Toggle User menu
+   */
+  toggleMenu() {
+    this.showOptions = !this.showOptions;
+  }
+
+  /**
+   * Sign out user
+   */
+  signOut(){
+    this._authService.signOut();
   }
 
 }

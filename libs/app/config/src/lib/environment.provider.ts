d9173ed448930645aa254f3367d1bc162f35ead9
pmutua <pmutua@live.com>
2023-06-14 23:59:07 +0300

    ci: fix environment import

--- libs/app/config/src/lib/environment.provider.ts
+++ libs/app/config/src/lib/environment.provider.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
-import { environment as env } from './environments/w';
+import { environment as env } from './environments/environment';
 
 // Dont import this in any module its for config purposes 
 import { Injectable } from '@angular/core';

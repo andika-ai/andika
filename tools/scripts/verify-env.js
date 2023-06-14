410b4f0411cc8b3c7beabd9b8f1236b961a271c7
pmutua <pmutua@live.com>
2023-06-14 23:51:59 +0300

    ci: verify env empty

--- tools/scripts/verify-env.js
+++ tools/scripts/verify-env.js
@@ -3,16 +3,14 @@ const path = require('path');
 
 const environmentFilePath = path.join(__dirname, '../../libs/app/config/src/lib/environments/environment.ts');
 
-fs.readFile(environmentFilePath, 'utf8', (err, data) => {
-  if (err) {
-    console.error('Error reading the file:', err);
-    process.exit(1);
-  }
-
+try {
+  const data = fs.readFileSync(environmentFilePath, 'utf8');
   if (data.trim().length > 0) {
-    console.error('Error: The environment file is not empty.');
-    process.exit(1);
+    throw new Error('Error: The environment file is not empty.');
   } else {
     console.log('The environment file is empty.');
   }
-});
+} catch (err) {
+  console.error('Error reading the file:', err);
+  process.exit(1);
+}
